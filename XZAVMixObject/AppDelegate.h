c87791ad4fd106185374b882763c8f572414c087
zhaoyongjie <18515381796@163.com>
2017-01-09 13:42:31 +0800

    Initial Commit

---
+++ XZAVMixObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZAVMixObject
+//
+//  Created by MYKJ on 17/1/9.
+//  Copyright © 2017年 zhaoyongjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
